68384d2e89f3431ad6fb4d661caa742471bfbc76
prlanzarin <prlanzarin@inf.ufrgs.br>
2019-03-22 16:32:21 +0000

    Initial working version, 0.0.1

---
+++ eavesdropper.js
@@ -0,0 +1,10 @@
+/*
+ * Paulo Renato Lanzarin
+ * (C) Copyright 2018 Mconf Tecnologia
+ *
+ */
+
+'use strict';
+
+const Logger = require('./lib/logger.js');
+const PP = require('./lib/eavesdropper-process.js');
